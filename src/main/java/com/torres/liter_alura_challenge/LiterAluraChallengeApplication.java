212ffc954d817505545cd6ab6c12e4778f3b1ed6
Yalith Torres <yalithdev@gmail.com>
2024-07-07 14:57:12 -0400

    Primer commit

---
+++ src/main/java/com/torres/liter_alura_challenge/LiterAluraChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.torres.liter_alura_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/torres/liter_alura_challenge/LiterAluraChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torres.liter_alura_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
