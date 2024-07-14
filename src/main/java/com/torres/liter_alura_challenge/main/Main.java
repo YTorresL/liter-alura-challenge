3bf1fd961bbc1e275e13a63f6c77e99df9653f17
Yalith Torres <yalithdev@gmail.com>
2024-07-14 02:33:25 -0400

    Menu finish

--- src/main/java/com/torres/liter_alura_challenge/main/Main.java
+++ src/main/java/com/torres/liter_alura_challenge/main/Main.java
@@ -10,23 +10,56 @@ public class Main {
     private final String BASE_URL = "https://gutendex.com/books/?search=";
 
     public void menu() {
-            var option = true;
-            while (option) {
+            var start = true;
+            while (start) {
                 System.out.println("""
+                        -------------------------------
+                        
                         1 - Search books
                         2 - List books registered
                         3 - List authors registered
                         4 - List books registered by language
                         5 - List books registered by download count
                         0 - Exit
+                        
+                        -------------------------------
                         """);
                 System.out.print("Choose an option: ");
 
+                var option = keyboard.nextInt();
+
                 switch (option) {
                     case 1 -> searchBooks();
-                    case 0 -> System.out.println("Bye!");
+                    case 2 -> listBooks();
+                    case 3 -> listAuthors();
+                    case 4 -> listBooksByLanguage();
+                    case 5 -> listBooksByDownloadCount();
+                    case 0 -> {
+                        System.out.println("Bye!");
+                        start = false;
+                    }
                     default -> System.out.println("Invalid option");
                 }
             }
         }
+
+    private void searchBooks() {
+        System.out.print("Enter the book title: ");
+        var title = keyboard.next();
+        var url = BASE_URL + title.replace(" ", "%20");
+        var json = apiQueries.getData(url);
+        System.out.println(json);
     }
+
+    private void listBooks() {
+    }
+
+    private void listAuthors() {
+    }
+
+    private void listBooksByLanguage() {
+    }
+
+    private void listBooksByDownloadCount() {
+    }
+}
